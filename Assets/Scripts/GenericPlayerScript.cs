7737cf3d2c911441ae639e3d86220ce7e93ebef4
PawelDziedzic <pawel.piotr.dziedzic@gmail.com>
2019-10-27 13:54:00 +0100

    Initial commit

---
+++ Assets/Scripts/GenericPlayerScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericPlayerScript : MonoBehaviour
+{
+    public GameObject enemy;
+    public Vector3 focusPoint;
+    protected Animator animator; 
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        focusPoint = enemy.transform.position;
+        animator = GetComponent<Animator>();
+
+    }
+
+    void FixedUpdate()
+    {
+        focusPoint = enemy.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TurnTowardsFocus();
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                animator.SetTrigger("ForwardTrigger");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                animator.SetTrigger("RightTrigger");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                animator.SetTrigger("LeftTrigger");
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                animator.SetTrigger("BackTrigger");
+        }
+
+    }
+
+    void TurnTowardsFocus()
+    {
+        //transform.LookAt(focusPoint);
+    }
+
+    void ClearAllTriggers()
+    {
+        for(int i=0; i<animator.parameterCount;i++)
+        {
+            animator.ResetTrigger(i);
+        }
+    }
+}
---
+++ Assets/Scripts/StrafeLeftScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrafeLeftScript : StateMachineBehaviour
+{
+    public GenericPlayerScript parentScript;
+    Transform tempTransform;
+    float degree;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        parentScript = animator.gameObject.GetComponent<GenericPlayerScript>();
+        tempTransform = animator.gameObject.GetComponent<Transform>();
+        degree = 6f / Vector3.Distance(tempTransform.position, parentScript.focusPoint);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        tempTransform.Translate(new Vector3(-0.1047f, 0f, 0f));
+        tempTransform.Rotate(new Vector3(0f, degree, 0f));
+        //tempTransform.LookAt(parentScript.focusPoint);
+        animator.rootPosition = tempTransform.position;
+        animator.rootRotation = tempTransform.rotation;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
